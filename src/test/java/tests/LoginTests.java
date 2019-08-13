310af026b7fa7ae344650c50ddf0693aecc4f4d0
Murad1990 <muradlatifov90@gmail.com>
2019-08-12 22:05:06 -0400

    After method added and one more test added

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -3,7 +3,9 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -11,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class LoginTests {
     WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
@@ -19,8 +21,19 @@ public class LoginTests {
 
     }
 
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
+    }
+
     @Test
     public void loginTest1(){
         driver.get("https://google.com");
     }
+
+    @Test
+    public void someTest(){
+        driver.get("https://amazon.com");
+    }
+
 }
